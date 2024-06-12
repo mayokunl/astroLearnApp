09835a8abd1a16f6067b6bc1c6df13a951458294
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2024-06-11 22:08:26 -0500

    Trying things

---
+++ src/main/java/com/astroLearnApp/astroLearn/AstroLearnApplication.java
@@ -0,0 +1,13 @@
+package com.astroLearnApp.astroLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astroLearnApp/astroLearn/AstroLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astroLearnApp.astroLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
